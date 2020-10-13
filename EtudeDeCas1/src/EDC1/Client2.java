14e3e6215a2a16d2141e767dd59432c193046029
hanane-dahi <65444324+hanane-dahi@users.noreply.github.com>
2020-10-13 15:11:21 +0100

    Update Client2.java

--- EtudeDeCas1/src/EDC1/Client2.java
+++ EtudeDeCas1/src/EDC1/Client2.java
@@ -7,6 +7,7 @@ public class Client2 {
 	public static void main(String[] args) {
 		int w=1;
 		Scanner clnt = new Scanner(System.in);
+		System.out.println("Vous pouvez toujours saisir id et l'age du client ");
 		while(w==1){
 			int id = clnt.nextInt();
 		    int age = clnt.nextInt();		
