47195098131e899ab9a0402449b5f4554df25832
hanane-dahi <65444324+hanane-dahi@users.noreply.github.com>
2020-10-13 16:14:34 +0100

    Update Terre.java

--- EtudeDeCas1/src/EDC1/Terre.java
+++ EtudeDeCas1/src/EDC1/Terre.java
@@ -1,62 +1,49 @@
 package EDC1;
 
 public class Terre {
-
-	private int c;
 	int age,id;
 	private Terre(int id, int age) {
 		this.id=id;
 		this.age=age;
-		
 	}
-	
 	
 	private static final Terre s;
 	static {
 		s=new Terre(0,0);
 	}
 	
-	
 	public static Terre getInstence() {
 		return s;
 	}
-	
-	
-	
-	
-	//créer
+	//crÃ©er
 	
 	public void creer(int id,int age) {
 		System.out.println("creation du client "+ id);
-		for(int i=1;i<=3;i++) {
-			++c;
-			System.out.println(".."+c);
+		for(int i=1;i<=5;i++) {
+			System.out.println("....");
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("le client  "+id+" a été crée");
+		System.out.println("le client  "+id+" a Ã©tÃ© crÃ©e");
 	
 	}
-	
-	
 	
 	//Ajouter
 
 	public void ajouter(int id,int age) {
 		System.out.println("ajouter le client "+ id);
-		for(int i=1;i<=3;i++) {
-			++c;
-			System.out.println(".."+c);
+		for(int i=1;i<=5;i++) {
+			System.out.println("....");
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("le client  "+id+" a été ajouté");
+		System.out.println("le client  "+id+" a Ã©tÃ© ajoutÃ©");
 	
 		
 
@@ -70,45 +57,5 @@ public class Terre {
 		System.out.println("id du client :  "+ id +"  et son age :"+ age);
 	}
 	
-	
-	//créer
-
-		public void creer2(int id,int age,int n) {
-			System.out.println("....creation du client "+ id);
-			for(int i=1;i<=3;i++) {
-				++c;
-				System.out.println(".."+c);
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			System.out.println("le client  "+id+" a été crée");
-		
-		}
-		
-		
-		
-		//Ajouter
-
-		public void ajouter2(int id,int age,int n) {
-			System.out.println("....ajouter le client "+ id);
-			for(int i=1;i<=3;i++) {
-				++c;
-				System.out.println(".."+c);
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			System.out.println("le client  "+id+" a été ajouté");
-		
-			
-
-		}
-	
-	
-	
 }
+	
